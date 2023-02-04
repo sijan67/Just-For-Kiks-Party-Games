eeab4179af6c2da49f04ce908190be3482f70982
sijan67 <sijuichi1@gmail.com>
2023-02-04 15:28:41 -0800

    Set up app

---
+++ Frontend/kiks-app/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Just For Kiks Party Games</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
