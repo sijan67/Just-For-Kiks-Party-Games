ba75ad2ade4bbc3344ec7b06b5157ffd6feeb335
Kd20011225 <84144367+Kd20011225@users.noreply.github.com>
2023-02-12 16:16:14 -0800

    Add cloud service

---
+++ cloud/app.js
@@ -0,0 +1,29 @@
+const http = require('http');
+
+const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+    'Access-Control-Max-Age': 2592000, 
+};
+
+http.createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    if (req.method === 'OPTIONS') {
+        res.writeHead(204, headers);
+        res.end();
+        return;
+    }
+
+    if (['GET', 'POST', 'PUT'].indexOf(req.method) > -1) {
+        req.on('data', chunk => {
+            console.log(chunk.toString());
+        });
+
+        res.writeHead(200, headers);
+        res.end('success');
+        return;
+    }
+
+    res.writeHead(405, headers);
+    res.end(`${req.method} is not allowed for the request.`);
+}).listen(8000);
--- cloud/mongo.js
+++ cloud/mongo.js
@@ -1,11 +1,4 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const mongoose = require('mongoose');
-
-
-mongoose.connect("mongodb://my_user:my_pwd@localhost:27017/mern", { useNewUrlParser: true });
-
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const questionSchema = new Schema({
     description: String,
@@ -13,15 +6,9 @@ const questionSchema = new Schema({
 });
 const Question = mongoose.model("question", questionSchema);
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-app.get('/favicon.ico', (req, res) => {
-    res.sendFile(path.join(`${__dirname}/favicon.ico`));
-});
-
-app.get('/questions', (req, res) => {
+mongoose.connect("mongodb://my_user:my_pwd@localhost:27017/mern", { useNewUrlParser: true });
+const http = require('http');
+http.createServer((req, res) => {
     Question.find({}, "description answer").then(questions => {
         if (questions !== null && questions.length > 0) {
             res.write(JSON.stringify(questions));
