b3b56aafcabc0fbbdc534d72cb775848bd5c955a
Ivan Guo <ivanguo248@gmail.com>
2023-04-06 11:46:14 -0700

    Changed roomCode into a string

--- cloud/api/models/User.js
+++ cloud/api/models/User.js
@@ -3,11 +3,7 @@ const mongoose = require('mongoose')
 const UserSchema = new mongoose.Schema({
     username: String,
     teamID: String,
-    roomCode: Number,
-    room: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Room'
-    }
+    roomCode: String,
 })
 
 module.exports = mongoose.model('User',UserSchema);
